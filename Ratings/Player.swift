e7a404e4444df42d75fce3e53e46c2d6585274f6
Oleg <olegpsiarovsky@gmail.com>
2019-01-24 02:32:15 +0200

    Create project

---
+++ Ratings/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Ratings
+//
+//  Created by mac on 24.01.2019.
+//  Copyright © 2019 Oleg_Psiarovsky. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    // MARK: - Properties
+    var name: String?
+    var game: String?
+    var rating: Int
+}
---
+++ Ratings/PlayersViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PlayersViewController.swift
+//  Ratings
+//
+//  Created by mac on 24.01.2019.
+//  Copyright © 2019 Oleg_Psiarovsky. All rights reserved.
+//
+
+import UIKit
+
+class PlayersViewController: UITableViewController {
+
+    // MARK: - Properties
+    var players = SampleData.generatePlayersData()
+
+}
+
+// MARK: - UITableViewDataSource
+extension PlayersViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return players.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell",
+                                                 for: indexPath) as! PlayerCell
+        
+        let player = players[indexPath.row]
+        cell.player = player
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+}
